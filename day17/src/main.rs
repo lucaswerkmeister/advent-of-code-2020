f8e119c0a3404e8ac0fe113d3bb90f3083e408bd
Lucas Werkmeister <mail@lucaswerkmeister.de>
2020-12-17 20:50:05 +0100

    Day 17, part 2

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -23,7 +23,7 @@ impl fmt::Display for ParseStateError {
 impl Error for ParseStateError {}
 
 #[derive(Clone, Debug, Eq, PartialEq)]
-struct State {
+struct State3d {
     active_cells: HashSet<(i64, i64, i64)>,
     cycles: u64,
     xs: RangeInclusive<i64>,
@@ -31,7 +31,7 @@ struct State {
     zs: RangeInclusive<i64>,
 }
 
-impl State {
+impl State3d {
     fn cycle(&self) -> Self {
         let mut potentially_active_cells = HashSet::with_capacity(self.active_cells.len() * 3);
         for (x, y, z) in &self.active_cells {
@@ -80,7 +80,7 @@ impl State {
     }
 }
 
-impl FromStr for State {
+impl FromStr for State3d {
     type Err = ParseStateError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
@@ -116,7 +116,7 @@ impl FromStr for State {
     }
 }
 
-impl fmt::Display for State {
+impl fmt::Display for State3d {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         for z in self.zs.clone() {
             write!(f, "z={}\n", z)?;
@@ -139,7 +139,165 @@ impl fmt::Display for State {
     }
 }
 
-fn part1(input: State) -> usize {
+#[derive(Clone, Debug, Eq, PartialEq)]
+struct State4d {
+    active_cells: HashSet<(i64, i64, i64, i64)>,
+    cycles: u64,
+    xs: RangeInclusive<i64>,
+    ys: RangeInclusive<i64>,
+    zs: RangeInclusive<i64>,
+    ws: RangeInclusive<i64>,
+}
+
+impl State4d {
+    fn cycle(&self) -> Self {
+        let mut potentially_active_cells = HashSet::with_capacity(self.active_cells.len() * 4);
+        for (x, y, z, w) in &self.active_cells {
+            for dx in -1..=1 {
+                for dy in -1..=1 {
+                    for dz in -1..=1 {
+                        for dw in -1..=1 {
+                            potentially_active_cells.insert((x + dx, y + dy, z + dz, w + dw));
+                        }
+                    }
+                }
+            }
+        }
+        let mut active_cells = HashSet::with_capacity(self.active_cells.len());
+        for (x, y, z, w) in potentially_active_cells {
+            let mut currently_active = false;
+            let mut currently_active_neighbors = 0;
+            for dx in -1..=1 {
+                for dy in -1..=1 {
+                    for dz in -1..=1 {
+                        for dw in -1..=1 {
+                            if self
+                                .active_cells
+                                .contains(&(x + dx, y + dy, z + dz, w + dw))
+                            {
+                                if dx == 0 && dy == 0 && dz == 0 && dw == 0 {
+                                    currently_active = true;
+                                } else {
+                                    currently_active_neighbors += 1;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if (currently_active
+                && (currently_active_neighbors == 2 || currently_active_neighbors == 3))
+                || (!currently_active && currently_active_neighbors == 3)
+            {
+                active_cells.insert((x, y, z, w));
+            }
+        }
+        Self {
+            cycles: self.cycles + 1,
+            xs: *active_cells
+                .iter()
+                .map(|(x, _y, _z, _w)| x)
+                .min()
+                .unwrap_or(&0)
+                ..=*active_cells
+                    .iter()
+                    .map(|(x, _y, _z, _w)| x)
+                    .max()
+                    .unwrap_or(&0),
+            ys: *active_cells
+                .iter()
+                .map(|(_x, y, _z, _w)| y)
+                .min()
+                .unwrap_or(&0)
+                ..=*active_cells
+                    .iter()
+                    .map(|(_x, y, _z, _w)| y)
+                    .max()
+                    .unwrap_or(&0),
+            zs: *active_cells
+                .iter()
+                .map(|(_x, _y, z, _w)| z)
+                .min()
+                .unwrap_or(&0)
+                ..=*active_cells
+                    .iter()
+                    .map(|(_x, _y, z, _w)| z)
+                    .max()
+                    .unwrap_or(&0),
+            ws: *active_cells
+                .iter()
+                .map(|(_x, _y, _z, w)| w)
+                .min()
+                .unwrap_or(&0)
+                ..=*active_cells
+                    .iter()
+                    .map(|(_x, _y, _z, w)| w)
+                    .max()
+                    .unwrap_or(&0),
+            active_cells,
+        }
+    }
+}
+
+impl From<State3d> for State4d {
+    fn from(state: State3d) -> Self {
+        Self {
+            active_cells: state
+                .active_cells
+                .into_iter()
+                .map(|(x, y, z)| (x, y, z, 0))
+                .collect(),
+            cycles: state.cycles,
+            xs: state.xs,
+            ys: state.ys,
+            zs: state.zs,
+            ws: 0..=0,
+        }
+    }
+}
+
+impl FromStr for State4d {
+    type Err = ParseStateError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let state3d: State3d = s.parse()?;
+        Ok(state3d.into())
+    }
+}
+
+impl fmt::Display for State4d {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for w in self.ws.clone() {
+            for z in self.zs.clone() {
+                write!(f, "z={}, w={}\n", z, w)?;
+                for y in self.ys.clone() {
+                    for x in self.xs.clone() {
+                        write!(
+                            f,
+                            "{}",
+                            if self.active_cells.contains(&(x, y, z, w)) {
+                                '#'
+                            } else {
+                                '.'
+                            }
+                        )?;
+                    }
+                    writeln!(f, "")?;
+                }
+            }
+        }
+        Ok(())
+    }
+}
+
+fn part1(input: State3d) -> usize {
+    let mut state = input;
+    for _i in 0..6 {
+        state = state.cycle();
+    }
+    state.active_cells.len()
+}
+
+fn part2(input: State4d) -> usize {
     let mut state = input;
     for _i in 0..6 {
         state = state.cycle();
@@ -148,8 +306,9 @@ fn part1(input: State) -> usize {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let input: State = fs::read_to_string("input")?.parse()?;
+    let input: State3d = fs::read_to_string("input")?.parse()?;
     println!("{}", part1(input.clone()));
+    println!("{}", part2(input.into()));
     Ok(())
 }
 
@@ -159,9 +318,9 @@ mod tests {
     use pretty_assertions::assert_eq;
 
     #[test]
-    fn test_state_cycle() {
+    fn test_state3d_cycle() {
         #[rustfmt::skip]
-        let input = State {
+        let input = State3d {
             active_cells: vec![
                 (1, 0, 0),
                 (2, 1, 0),
@@ -176,7 +335,7 @@ mod tests {
         };
         let output = input.cycle();
         #[rustfmt::skip]
-        assert_eq!(State {
+        assert_eq!(State3d {
             active_cells: vec![
                 (0, 1, -1),
                 (2, 2, -1),
@@ -198,14 +357,14 @@ mod tests {
     }
 
     #[test]
-    fn test_parse_state() {
+    fn test_parse_state3d() {
         let input = "\
 .#.
 ..#
 ###
 ";
         #[rustfmt::skip]
-        assert_eq!(Ok(State {
+        assert_eq!(Ok(State3d {
             active_cells: vec![
                 (1, 0, 0),
                 (2, 1, 0),
@@ -221,9 +380,9 @@ mod tests {
     }
 
     #[test]
-    fn test_display_state() {
+    fn test_display_state3d() {
         #[rustfmt::skip]
-        let state = State {
+        let state = State3d {
             active_cells: vec![
                 (1, 0, 0),
                 (2, 1, 0),
